4d12031a0c17bcd0bdbe5710963d184fddd6b68f
ShuwenHe <1201220707@pku.edu.cn>
2025-12-19 19:54:10 +0800

    feat:p1035

---
+++ p1035.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int k;
+	if(!(std::cin>>k)) return 0;
+	double sn = 0.0;
+	int n = 0;
+	while(sn<=k){
+		n++;
+		sn += 1.0/n;
+		std::cout<<sn<<std::endl;
+	}
+	return 0;
+}
